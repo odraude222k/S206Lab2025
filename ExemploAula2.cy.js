ae0e32eb01b16fd849bb4eae1e112575795726ed
Eduardo <160641722+odraude222k@users.noreply.github.com>
2025-04-28 19:01:10 -0300

    Add files via upload

--- ExemploAula2.cy.js
+++ ExemploAula2.cy.js
@@ -1,6 +1,6 @@
 ///<reference= cypress>
 describe("Testes da criação, registro e login", ()=>{
-  it("Teste criação de usuário com sucesso",()=>{
+  it.skip("Teste criação de usuário com sucesso",()=>{
     cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
     cy.get('.btn-link').click()
     cy.get('#firstName').type("dudu")
@@ -12,7 +12,7 @@ describe("Testes da criação, registro e login", ()=>{
 
   })
 
-  it("Teste criação de usuário com falha",()=>{
+  it.skip("Teste criação de usuário com falha",()=>{
     cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
     cy.get('.btn-link').click()
     cy.get('#firstName').type("dudu")
@@ -22,7 +22,7 @@ describe("Testes da criação, registro e login", ()=>{
 
   })  
 
-  it("Teste criação de login com sucesso",()=>{
+  it.skip("Teste criação de login com sucesso",()=>{
     let infos = criarUser()
     cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
     cy.get('#username').type(infos[0])
@@ -30,6 +30,24 @@ describe("Testes da criação, registro e login", ()=>{
     cy.get('.btn-primary').click()
     cy.get('h1.ng-binding').should("contain.text",infos[0])
   })
+
+  it("Teste criação de login com usuario deletado",()=>{
+    let infos = criarUser()
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+    cy.get('#username').type(infos[0])
+    cy.get('#password').type(infos[1])
+    cy.get('.btn-primary').click()
+    cy.get('h1.ng-binding').should("contain.text",infos[0])
+    cy.get('.ng-binding > a').click()
+    cy.get('.btn').click()
+    cy.get('#username').type(infos[0])
+    cy.get('#password').type(infos[1])
+    cy.get('.btn-primary').click()
+    cy.get('.ng-binding').should("contain.text","Username or password is incorrect")
+
+  })
+
+
 })
 
 function criarUser(){
